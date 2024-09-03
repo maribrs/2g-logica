a8d61e67999bca96c4619cca2195110fb9951dae
maribrs <mari.811312@gmail.com>
2024-09-03 13:39:29 -0300

    main

---
+++ assuntos/continue&breck.js
@@ -0,0 +1,14 @@
+     //let numero = 2
+     //while(numero !== 8){
+    //numero = math.floor(math.random() * 10)
+    //console.log(numero)
+    //}
+    
+    let i = 1
+    while(i <= 10){
+        if (i === 5){
+            continue;
+        }
+        console.log(i);
+        i++;
+    }
